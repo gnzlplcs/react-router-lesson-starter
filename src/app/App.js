24aec8bd169b834808f933845449de50334db111
gnzlplcs <palaciosgm@outlook.com>
2021-12-10 00:06:05 -0500

    lesson 7 adn 8 completed

--- src/app/App.js
+++ src/app/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 import About from "../components/About";
@@ -17,27 +17,29 @@ function App() {
     <Router>
       <Header />
       <main>
-        <Route path="/articles/:title" >
-          <Article />
-        </Route>
-        <Route path="/authors/:name">
-          <Author />
-        </Route>
-        <Route path="/about">
-          <About></About>
-        </Route>
-        <Route path="/sign-up">
-          <SignUp></SignUp>
-        </Route>
-        <Route path="/articles">
-          <Articles></Articles>
-        </Route>
-        <Route path="/categories">
-          <Categories></Categories>
-        </Route>
-        <Route path="/profile">
-          <Profile></Profile>
-        </Route>
+        <Switch>
+          <Route path="/articles/:title" >
+            <Article />
+          </Route>
+          <Route path="/authors/:name">
+            <Author />
+          </Route>
+          <Route path="/about">
+            <About></About>
+          </Route>
+          <Route path="/sign-up">
+            <SignUp></SignUp>
+          </Route>
+          <Route path="/articles">
+            <Articles></Articles>
+          </Route>
+          <Route path="/categories">
+            <Categories></Categories>
+          </Route>
+          <Route path="/profile">
+            <Profile></Profile>
+          </Route>
+        </Switch>
       </main>
       <Footer />
     </Router>
